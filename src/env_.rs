1addd44a95a3097acb57cf2c9be402229506a0bc
M Farkas-Dyck <strake888@gmail.com>
2018-12-02 01:33:25 -0800

    refactorize `Environ::get` slightly

--- src/env_.rs
+++ src/env_.rs
@@ -14,16 +14,12 @@ impl<'a> Environ<'a> {
     /// Returns `None` if the variable isn't.
     /// Returns `Some(None)` if the variable is, but the entry contains no '=' character.
     pub fn get<'b>(&self, s: &'b [u8]) -> Option<Option<&'a Nul<u8>>> {
-        for xs in self.0 {
-            let (k, v) = if let Some(i) = xs.iter().position(|&x| b'=' == x) {
-                let (k, v) = xs.split_at(i);
-                (k, Some(v.split_at(1).1))
-            } else {
-                (&xs[..], None)
-            };
-            if *s == *k { return Some(v); }
-        }
-        None
+        self.0.iter().find_map(|xs| {
+            let (k, v) = if let Some((k, _, v)) = try_split_on(xs, |&x| b'=' == x) {
+                (k, Some(v))
+            } else { (&xs[..], None) };
+            if *s == *k { Some(v) } else { None }
+        })
     }
 
     /// Get the value of the given environment variable.
@@ -40,4 +36,11 @@ impl<'a> Environ<'a> {
             }
         }
     }
+}
+
+fn try_split_on<A, F: Fn(&A) -> bool>(xs: &Nul<A>, p: F) -> Option<(&[A], &A, &Nul<A>)> {
+    if let Some(i) = xs.iter().position(p) {
+        let (xs, ys) = xs.split_at(i);
+        Some((xs, &ys[0], ys.split_at(1).1))
+    } else { None }
 }
